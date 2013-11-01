d3ba3058b234c065d0023a455a049edab13bc492
beiduo <vince@vincetekiMacBook-Air.local>
2013-11-01 14:21:14 +0800

    进一步改进，现在可以接受多种类型的参数作为查询对象了

--- 代码片段/event.targer.filter/targetfilter.js
+++ 代码片段/event.targer.filter/targetfilter.js
@@ -7,15 +7,41 @@ KISSY.add('targetfilter', function(S){
 
 	var $ = S.all;
 
-	return function(t, selector){
-		var t = $(t);
-		if (typeof selector === 'string'){
-			if (t.test(selector)){
+	return function(t, o){
+		var t = $(t), t2 = t, t3 = t;
+		if (typeof o === 'string'){
+			if (t.test(o)){
 				return t;
 			}
-			var elem = t.parent(selector);
+			var elem = t.parent(o);
 			if (elem && elem.length > 0){
 				return elem;
+			}
+			return t;
+		}
+		if (o.innerHTML){
+			if (t.getDomNode() === o){
+				return t;
+			}
+			while (t2.parent().length > 0) {
+				t2 = t2.parent();
+				if (t2.getDomNode() === o){
+					return t2;
+				}
+			}
+			return t;
+		}
+		if (o[0].innerHTML){
+			for (var i =0; i < o.length; i++){
+				if (t.getDomNode() === o[i]){
+					return t;
+				}
+				while (t3.parent().length > 0) {
+					t = t3.parent();
+					if (t3.getDomNode() === o[i]){
+						return t3;
+					}
+				}
 			}
 			return t;
 		}
